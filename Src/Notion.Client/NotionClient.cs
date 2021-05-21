abfcc536ba5c9efc989fb8b63b938b1e9aa357bf
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-05-22 01:32:30 +0530

    add Notion mangement client

---
+++ Src/Notion.Client/NotionClient.cs
@@ -0,0 +1,20 @@
+﻿namespace Notion.Client
+{
+    public class NotionClient : INotionClient
+    {
+        public NotionClient(string authToken)
+        {
+            AuthToken = authToken;
+            Users = new UsersClient(new RestClient(authToken));
+        }
+
+        public string AuthToken { get; }
+        public IUsersClient Users { get; }
+    }
+
+    public interface INotionClient
+    {
+        string AuthToken { get; }
+        IUsersClient Users { get; }
+    }
+}
