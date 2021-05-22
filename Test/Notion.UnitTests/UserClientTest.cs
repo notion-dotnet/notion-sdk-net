1c22966b4963c8e7863f1e6b6a127e4da39c8501
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-05-22 12:35:20 +0530

    add test case for retrieve a user

--- Test/Notion.UnitTests/UserClientTest.cs
+++ Test/Notion.UnitTests/UserClientTest.cs
@@ -7,19 +7,27 @@ namespace Notion.UnitTests
     public class UserClientTest
     {
         private readonly string _authToken;
+        private readonly IUsersClient _client;
 
         public UserClientTest()
         {
             _authToken = "<Token>";
+            _client = new UsersClient(new RestClient(_authToken));
         }
 
         [Fact(Skip = "Internal testing purpose")]
         public async Task ListUsers()
         {
-            var restClient = new RestClient(_authToken);
-            var client = new UsersClient(restClient);
-            var usersList = await client.ListAsync();
+            var usersList = await _client.ListAsync();
+            Assert.NotNull(usersList);
+        }
 
+        [Fact(Skip = "Internal testing purpose")]
+        public async Task RetrieveUser()
+        {
+            string userId = "5e37c1b4-630f-4e81-bd6b-296af31e345f";
+            var user = await _client.RetrieveAsync(userId);
+            Assert.NotNull(user);
         }
     }
 }
