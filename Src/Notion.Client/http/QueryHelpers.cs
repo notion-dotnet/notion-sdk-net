9b3fbcd7763c7bcc1493af8f3991eb3c8db66303
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-05-23 19:42:09 +0530

    fix QueryHelpers dependencies :hammer:

--- Src/Notion.Client/http/QueryHelpers.cs
+++ Src/Notion.Client/http/QueryHelpers.cs
@@ -1,8 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
-using System.Text.Encodings.Web;
 
 namespace Notion.Client.http
 {
@@ -77,9 +77,9 @@ namespace Notion.Client.http
             foreach (var parameter in queryParams)
             {
                 sb.Append(hasQuery ? '&' : '?');
-                sb.Append(UrlEncoder.Default.Encode(parameter.Key));
+                sb.Append(WebUtility.UrlEncode(parameter.Key));
                 sb.Append('=');
-                sb.Append(UrlEncoder.Default.Encode(parameter.Value));
+                sb.Append(WebUtility.UrlEncode(parameter.Value));
                 hasQuery = true;
             }
 
