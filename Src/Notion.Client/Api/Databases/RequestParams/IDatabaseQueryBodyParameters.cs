54d1cc6b97de05157fc29bdaae57aa913737bf43
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-06-02 22:11:13 +0530

    Move api request params into separate file & under client folder.

---
+++ Src/Notion.Client/Api/Databases/RequestParams/IDatabaseQueryBodyParameters.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Notion.Client
+{
+    public interface IDatabaseQueryBodyParameters : IPaginationParameters
+    {
+        Filter Filter { get; set; }
+        List<Sort> Sorts { get; set; }
+    }
+}
--- Src/Notion.Client/Models/Database/Database.cs
+++ Src/Notion.Client/Models/Database/Database.cs
@@ -4,30 +4,6 @@ using System.Collections.Generic;
 
 namespace Notion.Client
 {
-    public interface IDatabasesListQueryParmaters : IPaginationParameters
-    {
-    }
-
-    public class DatabasesListParameters : IDatabasesListQueryParmaters
-    {
-        public string StartCursor { get; set; }
-        public string PageSize { get; set; }
-    }
-
-    public interface IDatabaseQueryBodyParameters : IPaginationParameters
-    {
-        Filter Filter { get; set; }
-        List<Sort> Sorts { get; set; }
-    }
-
-    public class DatabasesQueryParameters : IDatabaseQueryBodyParameters
-    {
-        public Filter Filter { get; set; }
-        public List<Sort> Sorts { get; set; }
-        public string StartCursor { get; set; }
-        public string PageSize { get; set; }
-    }
-
     public class Database
     {
         public string Object => "database";
