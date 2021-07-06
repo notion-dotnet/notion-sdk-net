72d896b4b97d6e81c0d5bf19d3e003ad2e0d0f47
Daniil Teterin <danteterin@mail.ru>
2021-07-06 10:37:36 +0300

    Revert "Added Title property to Page" and added GetTitleProperty as extension
    This reverts commit cce11dde7a742c0ecd01ec56d56f4c56872404be.

---
+++ Src/Notion.Client/Extensions/QualityOfLifeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace Notion.Client.Extensions
+{
+    public static class QualityOfLifeExtensions
+    {
+        public static (string Key, TitlePropertyValue Value) GetTitleProperty(this Page page)
+        {
+            var keyValuePair = page.Properties.First(x =>
+                x.Value.Type == PropertyValueType.Title);
+            return (
+                Key: keyValuePair.Key, 
+                Value: (TitlePropertyValue) keyValuePair.Value);
+        }
+    }
+}
