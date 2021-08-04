ac095628ab29b67efd5f867d1b209807fffc3478
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-08-04 23:13:30 +0530

    add integration test for retrieve a page endpoint :white_check_mark:

--- Test/Notion.UnitTests/PagesClientTests.cs
+++ Test/Notion.UnitTests/PagesClientTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Notion.Client;
 using WireMock.ResponseBuilders;
 using Xunit;
@@ -13,6 +14,30 @@ namespace Notion.UnitTests
         public PagesClientTests()
         {
             _client = new PagesClient(new RestClient(ClientOptions));
+        }
+
+        [Fact]
+        public async Task RetrieveAsync()
+        {
+            var pageId = "251d2b5f-268c-4de2-afe9-c71ff92ca95c";
+            var path = ApiEndpoints.PagesApiUrls.Retrieve(pageId);
+            var jsonData = await File.ReadAllTextAsync("data/pages/PageObjectShouldHaveUrlPropertyResponse.json");
+
+            Server.Given(CreateGetRequestBuilder(path))
+                .RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithBody(jsonData)
+            );
+
+            var page = await _client.RetrieveAsync(pageId);
+
+            page.Url.Should().Be("https://www.notion.so/Avocado-251d2b5f268c4de2afe9c71ff92ca95c");
+            page.Id.Should().Be(pageId);
+            page.Parent.Type.Should().Be(ParentType.DatabaseId);
+            ((DatabaseParent)page.Parent).DatabaseId.Should().Be("48f8fee9-cd79-4180-bc2f-ec0398253067");
+            page.IsArchived.Should().BeFalse();
+
         }
 
         [Fact]
@@ -31,7 +56,8 @@ namespace Notion.UnitTests
 
             var page = await _client.RetrieveAsync(pageId);
 
-            Assert.Equal("https://www.notion.so/Avocado-251d2b5f268c4de2afe9c71ff92ca95c", page.Url);
+            page.Url.Should().Be("https://www.notion.so/Avocado-251d2b5f268c4de2afe9c71ff92ca95c");
+
         }
     }
 }
