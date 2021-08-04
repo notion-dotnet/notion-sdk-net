bf3fcdf6752a71090f474475803097a93a4172b3
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-08-04 21:44:21 +0530

    Add url property in Page object

---
+++ Test/Notion.UnitTests/PagesClientTests.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Notion.Client;
+using WireMock.ResponseBuilders;
+using Xunit;
+
+namespace Notion.UnitTests
+{
+    public class PagesClientTests : ApiTestBase
+    {
+        private readonly IPagesClient _client;
+
+        public PagesClientTests()
+        {
+            _client = new PagesClient(new RestClient(ClientOptions));
+        }
+
+        [Fact]
+        public async Task PageObjectShouldHaveUrlProperty()
+        {
+            var pageId = "251d2b5f-268c-4de2-afe9-c71ff92ca95c";
+            var path = ApiEndpoints.PagesApiUrls.Retrieve(pageId);
+            var jsonData = await File.ReadAllTextAsync("data/pages/PageObjectShouldHaveUrlPropertyResponse.json");
+
+            Server.Given(CreateGetRequestBuilder(path))
+                .RespondWith(
+                Response.Create()
+                    .WithStatusCode(200)
+                    .WithBody(jsonData)
+            );
+
+            var page = await _client.RetrieveAsync(pageId);
+
+            Assert.Equal("https://www.notion.so/Avocado-251d2b5f268c4de2afe9c71ff92ca95c", page.Url);
+        }
+    }
+}
