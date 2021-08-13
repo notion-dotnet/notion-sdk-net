89009c5e02228191ae0e96dd5e1be4b79b7d9e9b
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-08-14 00:28:31 +0530

    fix code factor issues :art:

--- examples/aspnet-core-app/Startup.cs
+++ examples/aspnet-core-app/Startup.cs
@@ -27,7 +27,6 @@ namespace aspnet_core_app
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
