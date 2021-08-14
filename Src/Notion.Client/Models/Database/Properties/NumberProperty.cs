9ffec6fdb1ba1738a9bc3b9d522f08ae3e5ed004
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-08-15 01:51:51 +0530

    Add support for new currency formats :sparkles:

--- Src/Notion.Client/Models/Database/Properties/NumberProperty.cs
+++ Src/Notion.Client/Models/Database/Properties/NumberProperty.cs
@@ -52,6 +52,66 @@ namespace Notion.Client
         Won,
 
         [EnumMember(Value = "yuan")]
-        Yuan
+        Yuan,
+
+        [EnumMember(Value = "hong_kong_dollar")]
+        HongKongDollar,
+
+        [EnumMember(Value = "new_zealand_dollar")]
+        NewZealandDollar,
+
+        [EnumMember(Value = "krona")]
+        Krona,
+
+        [EnumMember(Value = "norwegian_krone")]
+        NorwegianKrone,
+
+        [EnumMember(Value = "mexican_peso")]
+        MexicanPeso,
+
+        [EnumMember(Value = "rand")]
+        Rand,
+
+        [EnumMember(Value = "new_taiwan_dollar")]
+        NewTaiwanDollar,
+
+        [EnumMember(Value = "danish_krone")]
+        DanishKrone,
+
+        [EnumMember(Value = "zloty")]
+        Zloty,
+
+        [EnumMember(Value = "baht")]
+        Baht,
+        
+        [EnumMember(Value = "forint")]
+        Forint,
+
+        [EnumMember(Value = "koruna")]
+        Koruna,
+
+        [EnumMember(Value = "shekel")]
+        Shekel,
+
+        [EnumMember(Value = "chilean_peso")]
+        ChileanPeso,
+
+        [EnumMember(Value = "philippine_peso")]
+        PhilippinePeso,
+
+        [EnumMember(Value = "dirham")]
+        Dirham,
+
+        [EnumMember(Value = "colombian_peso")]
+        ColombianPeso,
+
+        [EnumMember(Value = "riyal")]
+        Riyal,
+
+        [EnumMember(Value = "ringgit")]
+        Ringgit,
+
+        [EnumMember(Value = "leu")]
+        Leu
     }
 }
