ad24fcc7391a5a48c9a2b1d949e09300004b2f5b
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-09-09 13:28:15 +0530

    Add an example to print properties values

---
+++ examples/print-database-property-name-and-values/Program.cs
@@ -0,0 +1,30 @@
+﻿using Notion.Client;
+
+var client = new NotionClient(new ClientOptions
+{
+    AuthToken = "<Token>"
+});
+
+var databasesQueryParameters = new DatabasesQueryParameters();
+var databaseId = "";
+var queryResult = await client.Databases.QueryAsync(databaseId, databasesQueryParameters);
+
+foreach (var result in queryResult.Results)
+{
+    Console.WriteLine("Page Id: " + result.Id);
+    foreach (var property in result.Properties)
+    {
+        Console.WriteLine(property.Key + " " + GetValue(property.Value));
+    }
+}
+
+object GetValue(PropertyValue p)
+{
+    switch (p)
+    {
+        case RichTextPropertyValue richTextPropertyValue:
+            return richTextPropertyValue.RichText.FirstOrDefault()?.PlainText;
+        default:
+            return null;
+    }
+}
