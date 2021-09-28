e0711c73ed36842865aa8a40a774c8babb2a7ecf
Hogne Vevle <hognemv@gmail.com>
2021-09-28 14:06:43 +0200

    Add StringEnumConverter

--- Src/Notion.Client/Api/Search/Parameters/SearchFilter.cs
+++ Src/Notion.Client/Api/Search/Parameters/SearchFilter.cs
@@ -1,7 +1,11 @@
-﻿namespace Notion.Client
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Notion.Client
 {
     public class SearchFilter
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public SearchObjectType Value { get; set; }
         public string Property => "object";
     }
--- Src/Notion.Client/Api/Search/Parameters/SearchSort.cs
+++ Src/Notion.Client/Api/Search/Parameters/SearchSort.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
 namespace Notion.Client
 {
     public class SearchSort
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public SearchDirection Direction { get; set; }
 
         public string Timestamp { get; set; }
