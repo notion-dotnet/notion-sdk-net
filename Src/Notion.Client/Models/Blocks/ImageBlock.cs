36e0ec2787151e0db8867455e6c46e50cfd6c01e
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-10-02 02:24:21 +0530

    Remove duplicate classes ♻️

--- Src/Notion.Client/Models/Blocks/ImageBlock.cs
+++ Src/Notion.Client/Models/Blocks/ImageBlock.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Runtime.Serialization;
-using JsonSubTypes;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+﻿using Newtonsoft.Json;
 
 namespace Notion.Client
 {
@@ -12,40 +8,5 @@ namespace Notion.Client
 
         [JsonProperty("image")]
         public FileObject Image { get; set; }
-    }
-
-    public enum FileType
-    {
-        [EnumMember(Value = "external")]
-        External,
-
-        [EnumMember(Value = "file")]
-        File
-    }
-
-    [JsonConverter(typeof(JsonSubtypes), "type")]
-    [JsonSubtypes.KnownSubType(typeof(UploadedFile), FileType.File)]
-    [JsonSubtypes.KnownSubType(typeof(ExternalFile), FileType.External)]
-    public abstract class FileObject
-    {
-        [JsonProperty("type")]
-        [JsonConverter(typeof(StringEnumConverter))]
-        public virtual FileType Type { get; set; }
-
-        [JsonProperty("url")]
-        public string Url { get; set; }
-    }
-
-    public class UploadedFile : FileObject
-    {
-        public override FileType Type => FileType.File;
-
-        [JsonProperty("expiry_time")]
-        public DateTime ExpiryTime { get; set; }
-    }
-
-    public class ExternalFile : FileObject
-    {
-        public override FileType Type => FileType.External;
     }
 }
