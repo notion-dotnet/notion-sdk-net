32119c6f5611810908d946eaf9fb6cd696a46bdb
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-10-01 21:08:42 +0530

    Fix relation property schema

--- Src/Notion.Client/Api/Databases/RequestParams/DatabasesCreateParameters/PropertySchema/RelationPropertySchema.cs
+++ Src/Notion.Client/Api/Databases/RequestParams/DatabasesCreateParameters/PropertySchema/RelationPropertySchema.cs
@@ -1,11 +1,23 @@
-using System;
+﻿using System;
 using Newtonsoft.Json;
 
 namespace Notion.Client
 {
     public class RelationPropertySchema : IPropertySchema
     {
-        [JsonProperty("database_id")]
-        public Guid DatabaseId { get; set; }
+        [JsonProperty("relation")]
+        public RelationInfo Relation { get; set; }
+
+        public class RelationInfo
+        {
+            [JsonProperty("database_id")]
+            public Guid DatabaseId { get; set; }
+
+            [JsonProperty("synced_property_id")]
+            public string SynchedPropertyId { get; set; }
+
+            [JsonProperty("synced_property_name")]
+            public string SynchedPropertyName { get; set; }
+        }
     }
 }
