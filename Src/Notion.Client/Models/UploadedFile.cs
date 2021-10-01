da39d0f45f035b988f83cfbaded3eb81d108587a
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-10-02 03:39:15 +0530

    Keep a single class per a file

---
+++ Src/Notion.Client/Models/UploadedFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class UploadedFile : FileObject
+    {
+        public override string Type => "file";
+
+        [JsonProperty("file")]
+        public Info File { get; set; }
+
+        public class Info
+        {
+            [JsonProperty("url")]
+            public string Url { get; set; }
+
+            [JsonProperty("expiry_time")]
+            public DateTime ExpiryTime { get; set; }
+        }
+    }
+}
