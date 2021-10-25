da52cacba7f1cbf4645ef98367293f9c77d2e784
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-10-25 22:21:57 +0530

    Add Http request and response trace logs 🔊

---
+++ Src/Notion.Client/RestClient/LoggingHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Notion.Client
+{
+    public class LoggingHandler : DelegatingHandler
+    {
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            Log.Trace("Request: {request}", request);
+
+            try
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+
+                Log.Trace("Response: {response}", response);
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Failed to get response: {exception}", ex);
+                throw;
+            }
+        }
+    }
+}
