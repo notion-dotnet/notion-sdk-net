c9bb91b230b1ea3c4ed63498aa40c9bdbfed4bfe
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-11-01 18:17:40 +0530

    Add support for Breadcrumb block type ✨

---
+++ Src/Notion.Client/Api/Blocks/RequestParams/BlocksUpdateParameters/UpdateBlocks/BreadcrumbUpdateBlock.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class BreadcrumbUpdateBlock : IUpdateBlock
+    {
+        public bool Archived { get; set; }
+
+        [JsonProperty("breadcrumb")]
+        public Data Breadcrumb { get; set; }
+
+        public class Data
+        {
+        }
+
+        public BreadcrumbUpdateBlock()
+        {
+            Breadcrumb = new Data();
+        }
+    }
+}
---
+++ Src/Notion.Client/Models/Blocks/BreadcrumbBlock.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class BreadcrumbBlock : Block
+    {
+        public override BlockType Type => BlockType.Breadcrumb;
+
+        [JsonProperty("breadcrumb")]
+        public Data Breadcrumb { get; set; }
+
+        public class Data
+        {
+
+        }
+    }
+}
