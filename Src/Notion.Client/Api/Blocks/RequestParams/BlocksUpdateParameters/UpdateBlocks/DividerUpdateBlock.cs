cc8ff36835ee30bbffc2d82c91477a4c447ef3cd
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-11-04 14:20:22 +0530

    Add support for Divider block type ✨

---
+++ Src/Notion.Client/Api/Blocks/RequestParams/BlocksUpdateParameters/UpdateBlocks/DividerUpdateBlock.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class DividerUpdateBlock : IUpdateBlock
+    {
+        public bool Archived { get; set; }
+
+        [JsonProperty("divider")]
+        public Data Divider { get; set; }
+
+        public class Data
+        {
+
+        }
+
+        public DividerUpdateBlock()
+        {
+            Divider = new Data();
+        }
+    }
+}
---
+++ Src/Notion.Client/Models/Blocks/DividerBlock.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class DividerBlock : Block
+    {
+        public override BlockType Type => BlockType.Divider;
+
+        [JsonProperty("divider")]
+        public Data Divider { get; set; }
+
+        public class Data
+        {
+        }
+    }
+}
