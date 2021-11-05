aaf5c7b464ac0d3668de43d66fe03f3c6478e9a5
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-11-05 13:26:51 +0530

    Add support to update equation block

---
+++ Src/Notion.Client/Api/Blocks/RequestParams/BlocksUpdateParameters/UpdateBlocks/EquationUpdateBlock.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class EquationUpdateBlock : UpdateBlock, IUpdateBlock
+    {
+        [JsonProperty("equation")]
+        public Data Equation { get; set; }
+
+        public class Data
+        {
+            [JsonProperty("expression")]
+            public string Expression { get; set; }
+        }
+    }
+}
