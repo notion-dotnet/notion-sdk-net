f65fe9bae0ba611136b6b05ba84d06abfc02efda
Vedant Koditkar <vedant.koditkar@outlook.com>
2021-11-05 13:09:05 +0530

    Add support to update bookmark block

---
+++ Src/Notion.Client/Api/Blocks/RequestParams/BlocksUpdateParameters/UpdateBlocks/BookmarkUpdateBlock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class BookmarkUpdateBlock : IUpdateBlock
+    {
+        public bool Archived { get; set; }
+
+        [JsonProperty("bookmark")]
+        public Data Bookmark { get; set; }
+
+        public class Data
+        {
+            [JsonProperty("url")]
+            public string Url { get; set; }
+
+            [JsonProperty("caption")]
+            public IEnumerable<RichTextBaseInput> Caption { get; set; }
+        }
+    }
+}
