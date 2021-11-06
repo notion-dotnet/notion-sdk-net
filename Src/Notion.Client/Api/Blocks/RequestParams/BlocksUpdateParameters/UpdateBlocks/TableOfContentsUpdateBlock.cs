3af7b984fe8b61986d2e36e4177cd887a1461d75
Mike <mike@hutility.com>
2021-11-05 22:09:06 -0400

    Add support for the table of contents block type

---
+++ Src/Notion.Client/Api/Blocks/RequestParams/BlocksUpdateParameters/UpdateBlocks/TableOfContentsUpdateBlock.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class TableOfContentsUpdateBlock : IUpdateBlock
+    {
+        public bool Archived { get; set; }
+
+        [JsonProperty("table_of_contents")]
+        public Data TableOfContents { get; set; }
+
+        public class Data
+        {
+        }
+
+        public TableOfContentsUpdateBlock()
+        {
+            TableOfContents = new Data();
+        }
+    }
+}
---
+++ Src/Notion.Client/Models/Blocks/TableOfContentsBlock.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class TableOfContentsBlock : Block
+    {
+        public override BlockType Type => BlockType.TableOfContents;
+
+        [JsonProperty("table_of_contents")]
+        public Data TableOfContents { get; set; }
+
+        public class Data
+        {
+        }
+    }
+}
