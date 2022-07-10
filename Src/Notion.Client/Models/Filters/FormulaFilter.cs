6d8a3452942dd65f47393cecbf231c762ce13a9e
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-07-11 02:23:40 +0530

    Rename Text prop to String in formula property filter

--- Src/Notion.Client/Models/Filters/FormulaFilter.cs
+++ Src/Notion.Client/Models/Filters/FormulaFilter.cs
@@ -9,14 +9,14 @@ namespace Notion.Client
 
         public FormulaFilter(
             string propertyName,
-            TextFilter.Condition text = null,
+            TextFilter.Condition @string = null,
             CheckboxFilter.Condition checkbox = null,
             NumberFilter.Condition number = null,
             DateFilter.Condition date = null)
         {
             Property = propertyName;
             Formula = new Condition(
-                text: text,
+                @string: @string,
                 checkbox: checkbox,
                 number: number,
                 date: date
@@ -25,8 +25,8 @@ namespace Notion.Client
 
         public class Condition
         {
-            [JsonProperty("text")]
-            public TextFilter.Condition Text { get; set; }
+            [JsonProperty("string")]
+            public TextFilter.Condition String { get; set; }
 
             [JsonProperty("checkbox")]
             public CheckboxFilter.Condition Checkbox { get; set; }
@@ -38,12 +38,12 @@ namespace Notion.Client
             public DateFilter.Condition Date { get; set; }
 
             public Condition(
-                TextFilter.Condition text = null,
+                TextFilter.Condition @string = null,
                 CheckboxFilter.Condition checkbox = null,
                 NumberFilter.Condition number = null,
                 DateFilter.Condition date = null)
             {
-                Text = text;
+                String = @string;
                 Checkbox = checkbox;
                 Number = number;
                 Date = date;
