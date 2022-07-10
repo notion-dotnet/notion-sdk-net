5cb1c5dc4447286e092b73a5f6cf79407bcbbde0
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-07-11 01:09:11 +0530

    Fix text filter properties and rename files

---
+++ Src/Notion.Client/Models/Filters/EmailFilter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class EmailFilter : SinglePropertyFilter
+    {
+        [JsonProperty("email")]
+        public TextFilter.Condition Email { get; set; }
+
+        public EmailFilter(
+            string propertyName,
+            string equal = null,
+            string doesNotEqual = null,
+            string contains = null,
+            string doesNotContain = null,
+            string startsWith = null,
+            string endsWith = null,
+            bool? isEmpty = null,
+            bool? isNotEmpty = null)
+        {
+            Property = propertyName;
+            Email = new TextFilter.Condition(
+                equal: equal,
+                doesNotEqual: doesNotEqual,
+                contains: contains,
+                doesNotContain: doesNotContain,
+                startsWith: startsWith,
+                endsWith: endsWith,
+                isEmpty: isEmpty,
+                isNotEmpty: isNotEmpty
+            );
+        }
+    }
+}
---
+++ Src/Notion.Client/Models/Filters/PhoneNumberFilter.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class PhoneNumberFilter : SinglePropertyFilter
+    {
+        [JsonProperty("phone_number")]
+        public TextFilter.Condition Text { get; set; }
+
+        public PhoneNumberFilter(
+            string propertyName,
+            string equal = null,
+            string doesNotEqual = null,
+            string contains = null,
+            string doesNotContain = null,
+            string startsWith = null,
+            string endsWith = null,
+            bool? isEmpty = null,
+            bool? isNotEmpty = null)
+        {
+            Property = propertyName;
+            Text = new TextFilter.Condition(
+                equal: equal,
+                doesNotEqual: doesNotEqual,
+                contains: contains,
+                doesNotContain: doesNotContain,
+                startsWith: startsWith,
+                endsWith: endsWith,
+                isEmpty: isEmpty,
+                isNotEmpty: isNotEmpty
+            );
+        }
+    }
+}
