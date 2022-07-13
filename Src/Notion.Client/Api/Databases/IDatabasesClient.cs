f83606e3c7c26bae1e97698145e460cd4b9356f6
Vedant Koditkar <vedant.koditkar@outlook.com>
2022-07-13 16:12:57 +0530

    Removed list database endpoint

--- Src/Notion.Client/Api/Databases/IDatabasesClient.cs
+++ Src/Notion.Client/Api/Databases/IDatabasesClient.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Notion.Client
 {
@@ -7,14 +6,6 @@ namespace Notion.Client
     {
         Task<Database> RetrieveAsync(string databaseId);
         Task<PaginatedList<Page>> QueryAsync(string databaseId, DatabasesQueryParameters databasesQueryParameters);
-
-        /// <summary>
-        /// List all Databases shared with the authenticated integration.
-        /// </summary>
-        /// <param name="databasesListParameters">database list request parameters.</param>
-        /// <returns>PaginatedList of databases.</returns>
-        [Obsolete("This endpoint is no longer recommended, use Search instead. This endpoint will only return explicitly shared pages, while search will also return child pages within explicitly shared pages. This endpoint's results cannot be filtered, while search can be used to match on page title.", false)]
-        Task<PaginatedList<Database>> ListAsync(DatabasesListParameters databasesListParameters = null);
 
         /// <summary>
         /// Creates a database as a subpage in the specified parent page, with the specified properties schema.
