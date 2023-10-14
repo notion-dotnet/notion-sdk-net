063723dd96d5120ec7b53a5a99644407494b35a9
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2023-10-14 23:51:17 +0530

    Add PageOrDatabase property in search response

--- Src/Notion.Client/Api/Search/ISearchClient.cs
+++ Src/Notion.Client/Api/Search/ISearchClient.cs
@@ -11,10 +11,14 @@ namespace Notion.Client
         ///     Searches all original pages, databases, and child pages/databases that are shared with the integration.
         ///     It will not return linked databases, since these duplicate their source databases.
         /// </summary>
-        /// <param name="parameters">Search filters and body parameters</param>
+        /// <param name="request">Search filters and body parameters</param>
+        /// <param name="cancellationToken"></param>
         /// <returns>
         ///     <see cref="PaginatedList{IObject}" />
         /// </returns>
-        Task<PaginatedList<IObject>> SearchAsync(SearchParameters parameters, CancellationToken cancellationToken = default);
+        Task<SearchResponse> SearchAsync(
+            SearchRequest request,
+            CancellationToken cancellationToken = default
+        );
     }
 }
