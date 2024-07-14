609e04dc8b9ef3969d3c214af95e5ddaf18e8b13
kosaku-hayashi <hayasi.mkmk128@gmail.com>
2024-07-14 17:01:31 +0900

    Added support for button properties.

---
+++ Src/Notion.Client/Models/Database/Properties/ButtonProperty.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Notion.Client
+{
+    public class ButtonProperty : Property
+    {
+        public override PropertyType Type => PropertyType.Button;
+
+        [JsonProperty("button")]
+        public Dictionary<string,object> Button { get; set; }
+    }
+}
