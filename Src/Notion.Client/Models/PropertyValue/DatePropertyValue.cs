cff7585c8aac2e09aacb66ccbfe306ba5f71bf03
Marian Zagoruiko <marian.zagoruiko@gmail.com>
2025-01-15 13:50:27 +0200

    feat: include time option for dates

--- Src/Notion.Client/Models/PropertyValue/DatePropertyValue.cs
+++ Src/Notion.Client/Models/PropertyValue/DatePropertyValue.cs
@@ -21,6 +21,7 @@ namespace Notion.Client
     /// <summary>
     ///     Date value object.
     /// </summary>
+    [JsonConverter(typeof(DateCustomConverter))]
     public class Date
     {
         /// <summary>
@@ -43,5 +44,94 @@ namespace Notion.Client
         /// </summary>
         [JsonProperty("time_zone")]
         public string TimeZone { get; set; }
+
+        /// <summary>
+        ///     Whether to include time
+        /// </summary>
+        public bool IncludeTime { get; set; } = true;
+    }
+
+    public class DateJsonObject
+    {
+        [JsonProperty("start")]
+        public string Start { get; set; }
+
+        [JsonProperty("end")]
+        public string End { get; set; }
+
+        [JsonProperty("time_zone")]
+        public string TimeZone { get; set; }
+    }
+
+    public class DateCustomConverter : JsonConverter<Date>
+    {
+        public override Date ReadJson(JsonReader reader, Type objectType, Date existingValue, bool hasExistingValue,
+            JsonSerializer serializer)
+        {
+            var jsonObject = serializer.Deserialize<DateJsonObject>(reader);
+
+            if (jsonObject == null)
+            {
+                return null;
+            }
+
+            var date = new Date
+            {
+                Start = ParseDateTime(jsonObject.Start, out bool includeTime),
+                End = ParseDateTime(jsonObject.End, out _),
+                TimeZone = jsonObject.TimeZone,
+                IncludeTime = includeTime,
+            };
+
+            return date;
+        }
+
+        public override void WriteJson(JsonWriter writer, Date value, JsonSerializer serializer)
+        {
+            if (value is null)
+            {
+                writer.WriteNull();
+
+                return;
+            }
+
+            writer.WriteStartObject();
+
+            if (value.Start.HasValue)
+            {
+                string startFormat = value.IncludeTime ? "yyyy-MM-ddTHH:mm:ss" : "yyyy-MM-dd";
+                writer.WritePropertyName("start");
+                writer.WriteValue(value.Start.Value.ToString(startFormat));
+            }
+
+            if (value.End.HasValue)
+            {
+                string endFormat = value.IncludeTime ? "yyyy-MM-ddTHH:mm:ss" : "yyyy-MM-dd";
+                writer.WritePropertyName("end");
+                writer.WriteValue(value.End.Value.ToString(endFormat));
+            }
+
+            if (!string.IsNullOrEmpty(value.TimeZone))
+            {
+                writer.WritePropertyName("time_zone");
+                writer.WriteValue(value.TimeZone);
+            }
+
+            writer.WriteEndObject();
+        }
+
+        private static DateTime? ParseDateTime(string dateTimeString, out bool includeTime)
+        {
+            includeTime = false;
+
+            if (string.IsNullOrEmpty(dateTimeString))
+            {
+                return null;
+            }
+
+            includeTime = dateTimeString.Contains("T") || dateTimeString.Contains(" ");
+
+            return DateTimeOffset.Parse(dateTimeString).UtcDateTime;
+        }
     }
 }
