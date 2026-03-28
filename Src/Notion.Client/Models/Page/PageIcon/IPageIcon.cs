c72933900ff190b62b37318afe9df115e00f94b5
Taco H. in den Bosch <taco@heddesit.nl>
2026-03-28 14:43:17 +0100

    Add support for native page icons

--- Src/Notion.Client/Models/Page/PageIcon/IPageIcon.cs
+++ Src/Notion.Client/Models/Page/PageIcon/IPageIcon.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 namespace Notion.Client
 {
     [JsonConverter(typeof(JsonSubtypes), "type")]
+    [JsonSubtypes.KnownSubType(typeof(NativeIconObject), "icon")]
     [JsonSubtypes.KnownSubType(typeof(EmojiPageIcon), PageIconTypes.Emoji)]
     [JsonSubtypes.KnownSubType(typeof(CustomEmojiPageIcon), PageIconTypes.CustomEmoji)]
     [JsonSubtypes.KnownSubType(typeof(FilePageIcon), PageIconTypes.File)]
---
+++ Src/Notion.Client/Models/Page/PageIcon/NativeIcon.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
+{
+    public class NativeIcon
+    {
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+
+    public class NativeIconObject : IPageIcon
+    {
+        [JsonProperty("icon")]
+        public NativeIcon Icon { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
