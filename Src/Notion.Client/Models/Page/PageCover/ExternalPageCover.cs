847f57a6fb88ac6be7f1c5de212dd9a92d9b1b42
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2026-04-26 09:36:30 +0530

    remove redundant JsonProperty attribute

--- Src/Notion.Client/Models/Page/PageCover/ExternalPageCover.cs
+++ Src/Notion.Client/Models/Page/PageCover/ExternalPageCover.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Notion.Client
 {
     public class ExternalPageCover : IPageCover
     {
-        [JsonProperty("type")]
         public string Type { get; set; } = "external";
 
         [JsonProperty("external")]
--- Src/Notion.Client/Models/Page/PageCover/FilePageCover.cs
+++ Src/Notion.Client/Models/Page/PageCover/FilePageCover.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Notion.Client
 {
     public class FilePageCover : IPageCover
     {
-        [JsonProperty("type")]
         public string Type { get; set; } = "file";
 
         [JsonProperty("file")]
