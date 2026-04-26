4209d484a294f251ff822489f80e92cc769d7128
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2026-04-26 11:54:11 +0530

    Add content object for unsupported block

--- Src/Notion.Client/Models/Blocks/UnsupportedBlock.cs
+++ Src/Notion.Client/Models/Blocks/UnsupportedBlock.cs
@@ -1,7 +1,18 @@
-﻿namespace Notion.Client
+﻿using Newtonsoft.Json;
+
+namespace Notion.Client
 {
     public class UnsupportedBlock : Block, IColumnChildrenBlock, INonColumnBlock
     {
         public override BlockType Type => BlockType.Unsupported;
+
+        [JsonProperty("unsupported")]
+        public UnsupportedBlockResponse Unsupported { get; set; }
+    }
+
+    public class UnsupportedBlockResponse
+    {
+        [JsonProperty("block_type")]
+        public string BlockType { get; set; }
     }
 }
