257675a794654bf7f9c5062f01b767478682d9bf
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2026-06-14 23:02:18 +0530

    feat: convert BlockType enum to extensible struct

--- Src/Notion.Client/Models/Blocks/BlockType.cs
+++ Src/Notion.Client/Models/Blocks/BlockType.cs
@@ -1,113 +1,102 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Runtime.Serialization;
+using System;
+using Newtonsoft.Json;
 
 namespace Notion.Client
 {
-    public enum BlockType
+    /// <summary>
+    /// Represents the type of a Notion block.
+    /// New values introduced by Notion are preserved as-is rather than causing a deserialization failure.
+    /// Use the <c>const string</c> members (e.g. <see cref="ParagraphValue"/>) when referencing a value
+    /// in a <c>[JsonSubtypes.KnownSubType]</c> attribute; use the <c>static readonly</c> fields
+    /// (e.g. <see cref="Paragraph"/>) everywhere else.
+    /// </summary>
+    [JsonConverter(typeof(ExtensibleEnumConverter<BlockType>))]
+    public readonly struct BlockType : IEquatable<BlockType>
     {
-        [EnumMember(Value = "paragraph")]
-        Paragraph,
+        private readonly string _value;
 
-        [EnumMember(Value = "heading_1")]
-        [SuppressMessage("ReSharper", "InconsistentNaming")]
-        Heading_1,
+        public BlockType(string value) => _value = value;
 
-        [EnumMember(Value = "heading_2")]
-        [SuppressMessage("ReSharper", "InconsistentNaming")]
-        Heading_2,
+        public const string ParagraphValue = "paragraph";
+        public const string Heading1Value = "heading_1";
+        public const string Heading2Value = "heading_2";
+        public const string Heading3Value = "heading_3";
+        public const string BulletedListItemValue = "bulleted_list_item";
+        public const string NumberedListItemValue = "numbered_list_item";
+        public const string ToDoValue = "to_do";
+        public const string ToggleValue = "toggle";
+        public const string ChildPageValue = "child_page";
+        public const string CodeValue = "code";
+        public const string ChildDatabaseValue = "child_database";
+        public const string EmbedValue = "embed";
+        public const string ImageValue = "image";
+        public const string VideoValue = "video";
+        public const string FileValue = "file";
+        public const string PDFValue = "pdf";
+        public const string BookmarkValue = "bookmark";
+        public const string EquationValue = "equation";
+        public const string BreadcrumbValue = "breadcrumb";
+        public const string DividerValue = "divider";
+        public const string AudioValue = "audio";
+        public const string TableOfContentsValue = "table_of_contents";
+        public const string CalloutValue = "callout";
+        public const string QuoteValue = "quote";
+        public const string ColumnValue = "column";
+        public const string ColumnListValue = "column_list";
+        public const string TemplateValue = "template";
+        public const string LinkToPageValue = "link_to_page";
+        public const string SyncedBlockValue = "synced_block";
+        public const string TableValue = "table";
+        public const string TableRowValue = "table_row";
+        public const string LinkPreviewValue = "link_preview";
+        public const string UnsupportedValue = "unsupported";
+        public const string TranscriptionValue = "transcription";
 
-        [EnumMember(Value = "heading_3")]
-        [SuppressMessage("ReSharper", "InconsistentNaming")]
-        Heading_3,
+        public static readonly BlockType Paragraph = new BlockType(ParagraphValue);
+        public static readonly BlockType Heading1 = new BlockType(Heading1Value);
+        public static readonly BlockType Heading2 = new BlockType(Heading2Value);
+        public static readonly BlockType Heading3 = new BlockType(Heading3Value);
+        public static readonly BlockType BulletedListItem = new BlockType(BulletedListItemValue);
+        public static readonly BlockType NumberedListItem = new BlockType(NumberedListItemValue);
+        public static readonly BlockType ToDo = new BlockType(ToDoValue);
+        public static readonly BlockType Toggle = new BlockType(ToggleValue);
+        public static readonly BlockType ChildPage = new BlockType(ChildPageValue);
+        public static readonly BlockType Code = new BlockType(CodeValue);
+        public static readonly BlockType ChildDatabase = new BlockType(ChildDatabaseValue);
+        public static readonly BlockType Embed = new BlockType(EmbedValue);
+        public static readonly BlockType Image = new BlockType(ImageValue);
+        public static readonly BlockType Video = new BlockType(VideoValue);
+        public static readonly BlockType File = new BlockType(FileValue);
+        public static readonly BlockType PDF = new BlockType(PDFValue);
+        public static readonly BlockType Bookmark = new BlockType(BookmarkValue);
+        public static readonly BlockType Equation = new BlockType(EquationValue);
+        public static readonly BlockType Breadcrumb = new BlockType(BreadcrumbValue);
+        public static readonly BlockType Divider = new BlockType(DividerValue);
+        public static readonly BlockType Audio = new BlockType(AudioValue);
+        public static readonly BlockType TableOfContents = new BlockType(TableOfContentsValue);
+        public static readonly BlockType Callout = new BlockType(CalloutValue);
+        public static readonly BlockType Quote = new BlockType(QuoteValue);
+        public static readonly BlockType Column = new BlockType(ColumnValue);
+        public static readonly BlockType ColumnList = new BlockType(ColumnListValue);
+        public static readonly BlockType Template = new BlockType(TemplateValue);
+        public static readonly BlockType LinkToPage = new BlockType(LinkToPageValue);
+        public static readonly BlockType SyncedBlock = new BlockType(SyncedBlockValue);
+        public static readonly BlockType Table = new BlockType(TableValue);
+        public static readonly BlockType TableRow = new BlockType(TableRowValue);
+        public static readonly BlockType LinkPreview = new BlockType(LinkPreviewValue);
+        public static readonly BlockType Unsupported = new BlockType(UnsupportedValue);
+        public static readonly BlockType Transcription = new BlockType(TranscriptionValue);
 
-        [EnumMember(Value = "bulleted_list_item")]
-        BulletedListItem,
+        public static implicit operator BlockType(string value) => new BlockType(value);
 
-        [EnumMember(Value = "numbered_list_item")]
-        NumberedListItem,
+        public static bool operator ==(BlockType left, BlockType right) => left.Equals(right);
+        public static bool operator !=(BlockType left, BlockType right) => !left.Equals(right);
 
-        [EnumMember(Value = "to_do")]
-        ToDo,
+        public bool Equals(BlockType other) =>
+            string.Equals(_value, other._value, StringComparison.Ordinal);
 
-        [EnumMember(Value = "toggle")]
-        Toggle,
-
-        [EnumMember(Value = "child_page")]
-        ChildPage,
-
-        [EnumMember(Value = "code")]
-        Code,
-
-        [EnumMember(Value = "child_database")]
-        ChildDatabase,
-
-        [EnumMember(Value = "embed")]
-        Embed,
-
-        [EnumMember(Value = "image")]
-        Image,
-
-        [EnumMember(Value = "video")]
-        Video,
-
-        [EnumMember(Value = "file")]
-        File,
-
-        [EnumMember(Value = "pdf")]
-        PDF,
-
-        [EnumMember(Value = "bookmark")]
-        Bookmark,
-
-        [EnumMember(Value = "equation")]
-        Equation,
-
-        [EnumMember(Value = "breadcrumb")]
-        Breadcrumb,
-
-        [EnumMember(Value = "divider")]
-        Divider,
-
-        [EnumMember(Value = "audio")]
-        Audio,
-
-        [EnumMember(Value = "table_of_contents")]
-        TableOfContents,
-
-        [EnumMember(Value = "callout")]
-        Callout,
-
-        [EnumMember(Value = "quote")]
-        Quote,
-
-        [EnumMember(Value = "column")]
-        Column,
-
-        [EnumMember(Value = "column_list")]
-        ColumnList,
-
-        [EnumMember(Value = "template")]
-        Template,
-
-        [EnumMember(Value = "link_to_page")]
-        LinkToPage,
-
-        [EnumMember(Value = "synced_block")]
-        SyncedBlock,
-
-        [EnumMember(Value = "table")]
-        Table,
-
-        [EnumMember(Value = "table_row")]
-        TableRow,
-
-        [EnumMember(Value = "link_preview")]
-        LinkPreview,
-
-        [EnumMember(Value = "unsupported")]
-        Unsupported,
-
-        [EnumMember(Value = "transcription")]
-        Transcription,
+        public override bool Equals(object obj) => obj is BlockType other && Equals(other);
+        public override int GetHashCode() => _value?.GetHashCode() ?? 0;
+        public override string ToString() => _value ?? string.Empty;
     }
 }
