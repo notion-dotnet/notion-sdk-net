efeb8e6eef41825ccf9f2b84b819a5fe28ef9a6e
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2026-06-14 18:58:02 +0530

    add integration tests for update page markdown, template and position

--- Test/Notion.IntegrationTests/PageWithPageParentTests.cs
+++ Test/Notion.IntegrationTests/PageWithPageParentTests.cs
@@ -4,12 +4,19 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Notion.Client;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace Notion.IntegrationTests;
 
 public class PageWithPageParentTests : IntegrationTestBase, IAsyncLifetime
 {
+    private readonly ITestOutputHelper _output;
     private Page _page = null!;
+
+    public PageWithPageParentTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
 
     public async Task InitializeAsync()
     {
@@ -66,5 +73,168 @@ public class PageWithPageParentTests : IntegrationTestBase, IAsyncLifetime
         );
 
         titleProperty.Results.First().As<TitlePropertyItem>().Title.PlainText.Should().Be("Page Title Updated");
+    }
+
+    [Fact]
+    public async Task CreateAsync_WithPageEndPosition_CreatesPageSuccessfully()
+    {
+        var page = await Client.Pages.CreateAsync(
+            PagesCreateParametersBuilder
+                .Create(new PageParentRequest { PageId = _page.Id })
+                .AddProperty("title", new TitlePropertyValue
+                {
+                    Title = new List<RichTextBase>
+                    {
+                        new RichTextText { Text = new Text { Content = "End Position Page" } }
+                    }
+                })
+                .SetPosition(new PageEndPosition())
+                .Build()
+        );
+
+        page.Should().NotBeNull();
+        page.Parent.Should().BeOfType<PageParent>()
+            .Which.PageId.Should().Be(_page.Id);
+    }
+
+    [Fact]
+    public async Task CreateAsync_WithPageStartPosition_CreatesPageSuccessfully()
+    {
+        var page = await Client.Pages.CreateAsync(
+            PagesCreateParametersBuilder
+                .Create(new PageParentRequest { PageId = _page.Id })
+                .AddProperty("title", new TitlePropertyValue
+                {
+                    Title = new List<RichTextBase>
+                    {
+                        new RichTextText { Text = new Text { Content = "Start Position Page" } }
+                    }
+                })
+                .SetPosition(new PageStartPosition())
+                .Build()
+        );
+
+        page.Should().NotBeNull();
+        page.Parent.Should().BeOfType<PageParent>()
+            .Which.PageId.Should().Be(_page.Id);
+    }
+
+    [Fact]
+    public async Task CreateAsync_WithAfterBlockPosition_CreatesPageAfterSibling()
+    {
+        // Create a sibling page first to use as the anchor
+        var sibling = await Client.Pages.CreateAsync(
+            PagesCreateParametersBuilder
+                .Create(new PageParentRequest { PageId = _page.Id })
+                .AddProperty("title", new TitlePropertyValue
+                {
+                    Title = new List<RichTextBase>
+                    {
+                        new RichTextText { Text = new Text { Content = "Sibling Page" } }
+                    }
+                })
+                .SetPosition(new PageEndPosition())
+                .Build()
+        );
+
+        var page = await Client.Pages.CreateAsync(
+            PagesCreateParametersBuilder
+                .Create(new PageParentRequest { PageId = _page.Id })
+                .AddProperty("title", new TitlePropertyValue
+                {
+                    Title = new List<RichTextBase>
+                    {
+                        new RichTextText { Text = new Text { Content = "After Sibling Page" } }
+                    }
+                })
+                .SetPosition(new AfterBlockPagePosition
+                {
+                    AfterBlock = new AfterBlockReference { Id = sibling.Id }
+                })
+                .Build()
+        );
+
+        page.Should().NotBeNull();
+        page.Parent.Should().BeOfType<PageParent>()
+            .Which.PageId.Should().Be(_page.Id);
+    }
+
+    [Fact]
+    public async Task CreateAsync_WithNoneTemplate_CreatesPageSuccessfully()
+    {
+        var page = await Client.Pages.CreateAsync(
+            PagesCreateParametersBuilder
+                .Create(new PageParentRequest { PageId = _page.Id })
+                .AddProperty("title", new TitlePropertyValue
+                {
+                    Title = new List<RichTextBase>
+                    {
+                        new RichTextText { Text = new Text { Content = "No Template Page" } }
+                    }
+                })
+                .SetTemplate(new NonePageTemplate())
+                .Build()
+        );
+
+        page.Should().NotBeNull();
+        page.Parent.Should().BeOfType<PageParent>()
+            .Which.PageId.Should().Be(_page.Id);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_WithEraseContent_ClearsPageContent()
+    {
+        var page = await Client.Pages.CreateAsync(
+            PagesCreateParametersBuilder
+                .Create(new PageParentRequest { PageId = _page.Id })
+                .SetMarkdown("# Heading\n\nSome content to erase.")
+                .Build()
+        );
+
+        var updated = await Client.Pages.UpdateAsync(page.Id, new PagesUpdateParameters
+        {
+            EraseContent = true
+        });
+
+        updated.Should().NotBeNull();
+        updated.Id.Should().Be(page.Id);
+
+        var markdown = await Client.Pages.RetrieveAsMarkdownAsync(
+            new RetrievePageAsMarkdownRequest { PageId = page.Id });
+
+        // After erase_content the page markdown should be empty or very minimal
+        markdown.Markdown.Should().NotContain("Some content to erase.");
+    }
+
+    [Fact]
+    public async Task UpdateAsync_WithIsLocked_LocksPage()
+    {
+        var page = await Client.Pages.CreateAsync(
+            PagesCreateParametersBuilder
+                .Create(new PageParentRequest { PageId = _page.Id })
+                .AddProperty("title", new TitlePropertyValue
+                {
+                    Title = new List<RichTextBase>
+                    {
+                        new RichTextText { Text = new Text { Content = "Page To Lock" } }
+                    }
+                })
+                .Build()
+        );
+
+        // Lock the page
+        var locked = await Client.Pages.UpdateAsync(page.Id, new PagesUpdateParameters
+        {
+            IsLocked = true
+        });
+
+        locked.Should().NotBeNull();
+
+        // Unlock for cleanup
+        await Client.Pages.UpdateAsync(page.Id, new PagesUpdateParameters
+        {
+            IsLocked = false,
+            InTrash = true
+        });
     }
 }
