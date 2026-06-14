56fa1b8116b15cccacbc2a894813e13d2fce00aa
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2026-06-14 23:03:43 +0530

    feat: convert ObjectType enum to extensible struct

--- Src/Notion.Client/Api/Search/Response/ISearchResponseObject.cs
+++ Src/Notion.Client/Api/Search/Response/ISearchResponseObject.cs
@@ -4,8 +4,9 @@ using Newtonsoft.Json;
 namespace Notion.Client
 {
     [JsonConverter(typeof(JsonSubtypes), "object")]
-    [JsonSubtypes.KnownSubType(typeof(Page), ObjectType.Page)]
-    [JsonSubtypes.KnownSubType(typeof(DataSource), ObjectType.DataSource)]
+    [JsonSubtypes.KnownSubType(typeof(Page), ObjectType.PageValue)]
+    [JsonSubtypes.KnownSubType(typeof(DataSource), ObjectType.DataSourceValue)]
+    [JsonSubtypes.FallBackSubTypeAttribute(typeof(UnknownObject))]
     public interface ISearchResponseObject : IObject
     {
     }
