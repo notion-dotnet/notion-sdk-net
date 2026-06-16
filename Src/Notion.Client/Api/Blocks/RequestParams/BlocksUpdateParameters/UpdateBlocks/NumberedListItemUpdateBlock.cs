097b92e27df44825f12b6a5c655541fc27cfda44
Vedant Koditkar <18693839+KoditkarVedant@users.noreply.github.com>
2026-06-16 15:48:56 +0530

    feat: add list_start_index and list_format to NumberedListItemBlock (#593)
    - Adds NumberedListFormat extensible struct (numbers, letters, roman)
    - Adds ListStartIndex (int?) and ListFormat (NumberedListFormat?) to:
      - NumberedListItemBlock.Info (read)
      - NumberedListItemBlockRequest.Info (create)
      - NumberedListItemUpdateBlock.Info (update)

--- Src/Notion.Client/Api/Blocks/RequestParams/BlocksUpdateParameters/UpdateBlocks/NumberedListItemUpdateBlock.cs
+++ Src/Notion.Client/Api/Blocks/RequestParams/BlocksUpdateParameters/UpdateBlocks/NumberedListItemUpdateBlock.cs
@@ -12,6 +12,12 @@ namespace Notion.Client
         {
             [JsonProperty("rich_text")]
             public IEnumerable<RichTextBaseInput> RichText { get; set; }
+
+            [JsonProperty("list_start_index")]
+            public int? ListStartIndex { get; set; }
+
+            [JsonProperty("list_format")]
+            public NumberedListFormat? ListFormat { get; set; }
         }
     }
 }
